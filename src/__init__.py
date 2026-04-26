59a2f6ee2fe760f228df08708ab34b828e8c66b1
Thaer Abushawer <thaer199@gmail.com>
2026-04-26 01:17:53 +0100

    Initialize __init__.py with module imports

---
+++ src/__init__.py
@@ -0,0 +1,4 @@
+from .chemistry    import build_chem, make_ocv, docv_dsoc
+from .machine1_dfn import run_dfn
+from .machine2_ekf import EKF, run_cosim
+from .machine3_pinn import PINNResidual, run_pinn
